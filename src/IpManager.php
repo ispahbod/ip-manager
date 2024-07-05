4583658e38b529a81cefcea47c7adfdf06086769
Hamed Hadi <hadispahbod@gmail.com>
2024-07-05 23:50:11 +0330

    Update project

--- src/IpManager.php
+++ src/IpManager.php
@@ -4,11 +4,11 @@ namespace Ispahbod\IpManager;
 
 class IpManager
 {
-    public function isValidIp($ip): bool
+    public static function isValidIp($ip): bool
     {
         return filter_var($ip, FILTER_VALIDATE_IP) !== false;
     }
-    public function getIpVersion($ip): ?int
+    public static function getIpVersion($ip): ?int
     {
         if (filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_IPV4)) {
             return 4;
@@ -20,12 +20,12 @@ class IpManager
 
         return null;
     }
-    public function getSubnetMask(string $ip): ?string
+    public static function getSubnetMask(string $ip): ?string
     {
         $parts = explode('/', $ip);
         return $parts[1] ?? null;
     }
-    public function getNetworkPortion(string $ip): ?string
+    public static function getNetworkPortion(string $ip): ?string
     {
         $subnetMask = $this->getSubnetMask($ip);
         if ($subnetMask === null) {
@@ -36,10 +36,24 @@ class IpManager
         $ipAddress = $parts[0];
         return long2ip((ip2long($ipAddress) & (-1 << (32 - (int)$subnetMask))));
     }
-    public function areIpsInSameSubnet(string $ip1, string $ip2): bool
+    public static function areIpsInSameSubnet(string $ip1, string $ip2): bool
     {
         $network1 = $this->getNetworkPortion($ip1);
         $network2 = $this->getNetworkPortion($ip2);
         return $network1 === $network2;
+    }
+
+    public static function generateFakeIpV4(): string
+    {
+        $ip = mt_rand(1, 254) . '.' . mt_rand(0, 255) . '.' . mt_rand(0, 255) . '.' . mt_rand(1, 254);
+        return $ip;
+    }
+
+    public static function generateFakeIpV6(): string
+    {
+        $ip = implode(':', array_map(function () {
+            return dechex(mt_rand(0, 65535));
+        }, range(1, 8)));
+        return $ip;
     }
 }
